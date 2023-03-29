7d836158a43852fe2853a0d3d8459532840085e0
Izzatullokh24 <86156093+Izzatullokh24@users.noreply.github.com>
2023-03-28 23:01:22 -0700

    Add files via upload

---
+++ project2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu2005/catkin_ws/assignment_1/src/project2
+# Build directory: /home/ubuntu2005/catkin_ws/assignment_1/build/project2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
