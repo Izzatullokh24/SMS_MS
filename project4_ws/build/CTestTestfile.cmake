66ba081c0786300e4514a095b2f80ad53d9e4a1b
Izzatullokh24 <86156093+Izzatullokh24@users.noreply.github.com>
2023-04-10 22:14:30 -0700

    Delete project4_ws/build directory

--- project4_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu2005/project4_ws/src
-# Build directory: /home/ubuntu2005/project4_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("project4")
